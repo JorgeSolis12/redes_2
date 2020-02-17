377df05fab88fe3980414ef1f1660d432f81f4eb
JorgeSolis12 <nirfana55@live.com>
2020-02-16 19:59:44 -0600

    lee archivo pero aun está sin reconocer

--- practica1.py
+++ practica1.py
@@ -1,11 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import threading
 
-num = 20
+#Listas con las palabras con sus variantes
+alegria = {Alegría,Alegria,alegria, ALEGRIA, ALEGRÍA}
+amor = {Amor, AMOR, amor}
+ira = {IRA, Ira, ira}
+frustracion ={frustracion, frustración, Frustración, FRUSTRACIÓN, FRUSTRACION}
+estres = {estres, estrés, ESTRES, Estres}
+enojo = {enojo, ENOJO, Enojo}
 
+#Cambiar el número para el número de hilos
+num = 1
+
+#funcion para crear hilos
 def worker(i):
-    """funcion que realiza el trabajo en el thread"""
+    repetidas_enojo = 0
+    repetidas_frust = 0
+    repetidas_estres = 0
+    repetidas_amor = 0
+    repetidas_ira = 0
+    repetidas_alegria = 0
+
     print("Hola mundo " + str(i+1))
+    dir = "/home/jorgesolis/redes_2/libros/"+str(i+1)+".txt"
+    archivo = open(dir, "r")
+    for linea in archivo.readlines():
+        print (linea) 
+    archivo.close()
     return
+
 threads = list()
 for i in range(num):
     t = threading.Thread(target=worker, args=(i,))
